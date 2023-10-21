3cf66fadca5503eec3080b960a57e6598ae41ec9
Andre Gomes <andregrps3@gmail.com>
2023-10-21 18:47:46 -0300

    pegando os dados dos campos de textos e preparando para fazer a requisicao na api

--- wissen/lib/ui/pages/login_page.dart
+++ wissen/lib/ui/pages/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:wissen/ui/pages/cliente/selecao_servico_page.dart';
 import 'package:wissen/ui/widgets/utils.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
 class LoginPage extends StatefulWidget {
   @override
@@ -8,12 +10,14 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  var usuario = Usuario();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Login')),
       body: Container(
-        // degrade suave de cinza cinza escuro
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topCenter,
@@ -27,35 +31,63 @@ class _LoginPageState extends State<LoginPage> {
         child: Center(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const CircleAvatar(
-                  radius: 100,
-                  backgroundImage: AssetImage('assets/images/wissen_logo.jpeg'),
-                ),
-                SizedBox(height: 30),
-                const TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Login',
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const CircleAvatar(
+                    radius: 100,
+                    backgroundImage:
+                        AssetImage('assets/images/wissen_logo.jpeg'),
                   ),
-                ),
-                SizedBox(height: 10),
-                const TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Senha',
+                  SizedBox(height: 30),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Login',
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter your login';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      usuario.login = value!;
+                    },
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: ElevatedButton(
-                    onPressed: _realizarLogin,
-                    child: const Text('Entrar', style: TextStyle(fontSize: 20)),
+                  SizedBox(height: 10),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Senha',
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter your password';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      usuario.senha = value!;
+                    },
                   ),
-                ),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState!.save(); // Save the form data
+                          _realizarLogin(usuario.login, usuario.senha);
+                        }
+                      },
+                      child:
+                          const Text('Entrar', style: TextStyle(fontSize: 20)),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
@@ -63,8 +95,26 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  void _realizarLogin() {
-    // realiza o login
-    showPage(context, SelecaoServicosPage());
+  void _realizarLogin(String login, String senha) async {
+    var url = Uri.https('urlBase', '/login/$login/senha/$senha');
+
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var jsonResponse =
+          convert.jsonDecode(response.body) as Map<String, dynamic>;
+      var autenticado = jsonResponse['autenticacao'];
+      if (autenticado == true) {
+        showPage(context, SelecaoServicosPage());
+      } else {
+        print('Login ou senha inválidos');
+      }
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
   }
+}
+
+class Usuario {
+  late String login;
+  late String senha;
 }
